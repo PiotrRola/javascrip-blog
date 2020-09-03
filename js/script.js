5fa5bccaded7b2cc3613cbc01b29bb3abea53608
Piotr Rola <rolapiotr@interia.pl>
2020-09-03 19:34:55 +0200

    dokończony Handler eventu

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 const titleClickHandler = function(event){
     const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event)
+    console.log(event);
+    event.preventDefault();
     
     /* [DONE]remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
@@ -12,7 +13,7 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
         }
   
-    /*[IN PROGRESS]add class 'active' to the clicked link */
+    /*[DONE]add class 'active' to the clicked link */
         console.log('clickedElement:', clickedElement);
         clickedElement.classList.add('active');
     /*[DONE] remove class 'active' from all articles */
@@ -23,10 +24,14 @@ const titleClickHandler = function(event){
         }
   
     /* get 'href' attribute from the clicked link */
-  
+        const articleSelector = clickedElement.getAttribute('href')
+        console.log(articleSelector)
+   
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+        const targetArticle = document.querySelector(clickedElement.getAttribute('href'))
+        console.log(targetArticle)
     /* add class 'active' to the correct article */
+        targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
