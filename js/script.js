cde00a56873105f2136f6822d84e4966cbdf91d0
Piotr Rola <rolapiotr@interia.pl>
2020-09-14 18:52:22 +0200

    Kod przed etapem podpięcia linków

--- js/script.js
+++ js/script.js
@@ -1,10 +1,51 @@
 'use strict';
+{const titleClickHandler = function(event){
+  const clickedElement = this;
+  console.log('Link was clicked!');
+  console.log(event);
+  event.preventDefault();
+  
+  /* [DONE]remove class 'active' from all article links  */
+      const activeLinks = document.querySelectorAll('.titles a.active');
 
+      for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+      }
+
+  /*[DONE]add class 'active' to the clicked link */
+      console.log('clickedElement:', clickedElement);
+      clickedElement.classList.add('active');
+  /*[DONE] remove class 'active' from all articles */
+      const activeArticles = document.querySelectorAll('.posts article');
+
+      for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+      }
+
+  /* [DONE]get 'href' attribute from the clicked link */
+      const articleSelector = clickedElement.getAttribute('href')
+      console.log(articleSelector)
+ 
+  /* [DONE]find the correct article using the selector (value of 'href' attribute) */
+      const targetArticle = document.querySelector(clickedElement.getAttribute('href'))
+      console.log(targetArticle)
+  /* [DONE]add class 'active' to the correct article */
+      targetArticle.classList.add('active');
+}
+const links = document.querySelectorAll('.titles a');
+  console.log(links)
+  for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+
+
+}
 
 
 const optArticleSelector ='.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
   function generateTitleLinks(){
   
 
@@ -56,49 +97,38 @@ const optArticleSelector ='.post',
     /* insert link into titleList */
     titleList.innerHTML = html;
     
-    {const titleClickHandler = function(event){
-      const clickedElement = this;
-      console.log('Link was clicked!');
-      console.log(event);
-      event.preventDefault();
-      
-      /* [DONE]remove class 'active' from all article links  */
-          const activeLinks = document.querySelectorAll('.titles a.active');
   
-          for(let activeLink of activeLinks){
-          activeLink.classList.remove('active');
-          }
-    
-      /*[DONE]add class 'active' to the clicked link */
-          console.log('clickedElement:', clickedElement);
-          clickedElement.classList.add('active');
-      /*[DONE] remove class 'active' from all articles */
-          const activeArticles = document.querySelectorAll('.posts article');
-  
-          for(let activeArticle of activeArticles){
-          activeArticle.classList.remove('active');
-          }
-    
-      /* [DONE]get 'href' attribute from the clicked link */
-          const articleSelector = clickedElement.getAttribute('href')
-          console.log(articleSelector)
-     
-      /* [DONE]find the correct article using the selector (value of 'href' attribute) */
-          const targetArticle = document.querySelector(clickedElement.getAttribute('href'))
-          console.log(targetArticle)
-      /* [DONE]add class 'active' to the correct article */
-          targetArticle.classList.add('active');
-    }
-    const links = document.querySelectorAll('.titles a');
-      console.log(links)
-      for(let link of links){
-      link.addEventListener('click', titleClickHandler);
-    }
-    
-    
-  }
     
     
   }
 
 generateTitleLinks();
+
+
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
